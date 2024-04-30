2eeaa741434a5a376781b7166fa44514ea93373a
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-04-30 20:13:23 +0000

    FIX: COMEÇO

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Course from "./components/Course";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
    <div>
@@ -8,6 +9,7 @@ export default function Home() {
     <Header/>
     <Banner/>
     <Course/>
+    <Footer/>
    </div>
   );
 }
