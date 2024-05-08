316192eea427a7d40344df88c03bbd4ad042995f
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-05-08 00:19:12 +0000

    fix: aside

---
+++ app/admin/components/Aside.tsx
@@ -0,0 +1,11 @@
+export default function Aside(){
+    return(
+        <div className="flex flex-col size-2/6">
+     
+        <a href="/admin">Home</a>
+        <a href="/admin/student">Estudantes</a>
+        
+        </div>
+    )
+    
+    }
